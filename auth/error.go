efd458b0f6b05cb542fb5c179e1115e791076b0c
Vamsi-BJE <eppevamsi@gmail.com>
2025-12-24 20:16:50 +0530

    error.go
    Remove outdated comments regarding login error detection.

--- auth/error.go
+++ auth/error.go
@@ -2,10 +2,7 @@ package auth
 
 import "github.com/go-rod/rod"
 
-// HasLoginError detects visible login errors (wrong email/password).
-// It does NOT detect CAPTCHA or checkpoints.
 func HasLoginError(page *rod.Page) bool {
-	// LinkedIn uses role="alert" for credential errors
 	_, err := page.Element("div[role='alert']")
 	return err == nil
 }
